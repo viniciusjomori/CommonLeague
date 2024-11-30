14e3d01b01c08361c8d3cb6849d9fbfc86d59d1b
Vinicius Jomori <yuitivinicius@gmail.com>
2024-11-30 17:59:37 -0300

    fix image link

--- src/main/java/br/com/jrr/apiTest/User/Enum/UserProfile.java
+++ src/main/java/br/com/jrr/apiTest/User/Enum/UserProfile.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import br.com.jrr.apiTest.User.DTO.UserProfileDTO;
 
 public enum UserProfile {
-    RED("https://s2-techtudo.glbimg.com/c-EmZ-_KhZUSd3gExFL5aoecgHw=/0x0:695x471/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/R/j/rSa1InQW6QKu8AqlIZnw/2016-07-21-valor.jpg"),
+    RED("https://github.com/NathanNevesPrates/CommonLeagueImages/blob/main/user/generic_user_red.jpeg?raw=true"),
     BLUE("https://github.com/NathanNevesPrates/CommonLeagueImages/blob/main/user/generic_user_blue.jpeg?raw=true"),
     YELLOW("https://github.com/NathanNevesPrates/CommonLeagueImages/blob/main/user/generic_user_yellow.jpeg?raw=true");
 
