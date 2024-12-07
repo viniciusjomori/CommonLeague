1e87e77a395061b2f14b29bf41fea613fe0b15cc
Vinicius Jomori <yuitivinicius@gmail.com>
2024-12-07 13:35:11 -0300

    cors config

--- src/main/java/br/com/jrr/apiTest/Security/SecurityConfig.java
+++ src/main/java/br/com/jrr/apiTest/Security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package br.com.jrr.apiTest.Security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.server.ResponseStatusException;
 
 import br.com.jrr.apiTest.Security.Filters.CaptainFilter;
@@ -42,6 +47,7 @@ public class SecurityConfig {
             .csrf(csrf -> {
                 csrf.disable();
             })
+            .cors(c -> c.configurationSource(corsConfigurationSource()))
             .headers(h -> {
                 h.frameOptions(fo -> {
                     fo.disable();
@@ -62,6 +68,18 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setExposedHeaders(Arrays.asList("Content-Type", "Authorization", "X-Amz-Date", "X-Api-Key", "X-Amz-Security-Token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
